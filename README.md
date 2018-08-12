010617884ad4dca8f2937c07a175b6499793f2ec
qh <qh@ivy.com>
2018-08-12 23:32:43 +0800

    上滑 加载 和 点击详情. finish

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# fluttertime
-flutter
+### flutterAndroidClient
+仿 知乎日报 的 flutter 的 例子.只兼容了 Android . 未考虑 iOS, 实现了日报 的列表页 和 详情页.
+
+<div align="center">
+    <img src="./install/list.png" height="330" width="190" >
+    <img src="./install/detail.png" height="330" width="190">
+</div>
+
+
+### 运行
+安装 官方文档 配置 flutter;
+使用 master分支安装, flutter 版本 为 0.58
+
+flutter run 
+
+### 结构
+列表页 与 详情 也 都位于 `/zhihu/MainPage.dart`
+
+目前 详情页. flutter 官方不 支持 webView, 所以加载的 html 有些标签无法显示;
+
+列表页 上滑加载 未实现完整逻辑 ,暂时 只是与 下拉刷新相同 逻辑;暂时搁置
+
+重构 将 MainPage 分为 列表页,详情页 和 ApiUtil; 暂时搁置.
+ 
--- lib/zhihu/MainPage.dart
+++ lib/zhihu/MainPage.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 // import 'package:dio/dio.dart';
+import 'package:flutter_html_view/flutter_html_view.dart';
 
 ///
 ///1. 路由  MaterialPageRoute
@@ -25,6 +26,7 @@ class MainView extends StatefulWidget {
 }
 
 class MainList extends State<MainView> {
+  final newsUrl = "http://news-at.zhihu.com/api/4/news/";
   final lastUrl = "http://news-at.zhihu.com/api/4/news/latest";
   final beforeUrl = "http://news.at.zhihu.com/api/4/news/before/";
 
@@ -46,16 +48,64 @@ class MainList extends State<MainView> {
   }
 
   Future<String> getStoryListJson() async {
+    print("httpList------");
     var httpClient = new HttpClient();
     var request = await httpClient.getUrl(Uri.parse(lastUrl));
     var response = await request.close();
     return await response.transform(utf8.decoder).join();
   }
 
+  Future<String> getStoryJson(var id) async {
+    print("httpStory------");
+    var httpClient = new HttpClient();
+    var request = await httpClient.getUrl(Uri.parse(newsUrl+id.toString()));
+    var response = await request.close();
+    return await response.transform(utf8.decoder).join();
+  }
+
   Future<Null> loadData() async {
+    print("loadData------");
     await getStoryListJson(); //注意await关键字
     if (!mounted) return; //异步处理，防止报错
     setState(() {}); //什么都不做，只为触发RefreshIndicator的子控件刷新
+  }
+
+  clickStory(var id ){
+    print("clickStory");
+    print(id);
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context){
+          return new FutureBuilder(
+            future: getStoryJson(id),
+            builder: (BuildContext context,AsyncSnapshot snapshot){
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return new Center(
+                    child: new Card(
+                      child: new Text('loading...'), //在页面中央显示正在加载
+                    ),
+                  );
+                default:
+                return new Scaffold(
+                  appBar: new AppBar(
+                    title: new Text(jsonDecode(snapshot.data)['title']),
+                  ),
+                  body: new SingleChildScrollView(
+                    child: new Center(
+                      child: new HtmlView(
+                        data: jsonDecode(snapshot.data)['body'],
+                      ),
+                    ),
+                  ),
+                );
+              }
+            },
+          );
+        }
+      )
+    );
   }
 
   @override
@@ -79,7 +129,7 @@ class MainList extends State<MainView> {
             case ConnectionState.waiting: //get正在执行时
               return new Center(
                 child: new Card(
-                  child: new Text('loading...'), //在页面中央显示正在加载
+                  child: new Text(''), //在页面中央显示正在加载
                 ),
               );
             default:
@@ -100,14 +150,12 @@ class MainList extends State<MainView> {
   Widget storyList(BuildContext context, AsyncSnapshot snapshot) {
     print(snapshot.data);
     List values;
-    values = jsonDecode(snapshot.data)['stories'] != null
-        ? jsonDecode(snapshot.data)['stories']
-        : [''];
+    values = jsonDecode(snapshot.data)['stories'] != null? jsonDecode(snapshot.data)['stories'] : [''];
     switch (values.length) {
       case 1: //没有获取到数据，则返回请求失败的原因
         return new Center(
           child: new Card(
-            child: new Text(jsonDecode(snapshot.data)['reason']),
+            child: new Text(jsonDecode(snapshot.data)['message']),
           ),
         );
       default:
@@ -115,7 +163,7 @@ class MainList extends State<MainView> {
           padding: const EdgeInsets.all(4.0),
           itemCount: values == null ? 0 : values.length,
           itemBuilder: (context, i) {
-            // if (i.isOdd) return new Divider();
+            if (i.isOdd) return new Divider();
             return getStoryList(values[i]);
           },
           controller: scrollController,
@@ -126,25 +174,28 @@ class MainList extends State<MainView> {
 
   Widget getStoryList(story) {
     print(story['title']);
-    return new Container(
-      constraints: new BoxConstraints.expand(
-        height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 100.0,
-      ),
-      padding: const EdgeInsets.all(8.0),
-      color: Colors.grey[100],
-      alignment: Alignment.centerLeft,
-      child: new Row(
-        children: <Widget>[
-          new Expanded(
-            child: new Image.network(
-              story['images'][0],
+    return new GestureDetector(
+      onTap: () { clickStory(story['id']); },
+      child: new Container(
+        constraints: new BoxConstraints.expand(
+          height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 100.0,
+        ),
+        padding: const EdgeInsets.all(8.0),
+        color: Colors.grey[100],
+        alignment: Alignment.centerLeft,
+        child: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new Image.network(
+                story['images'][0],
+              ),
             ),
-          ),
-          new Expanded(
-            child: new Text(story['title'], textAlign: TextAlign.center),
-          ),
-        ],
-      ),
+            new Expanded(
+              child: new Text(story['title'], textAlign: TextAlign.center),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
